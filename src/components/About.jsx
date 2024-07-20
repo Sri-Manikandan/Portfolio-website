c4b660c66651dcc1d3382bff042a0ad0f9cac8c4
Sri Manikandan R <srimanikandandev@gmail.com>
2024-07-20 08:41:39 +0530

    more responsive styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import aboutImg from '../assets/manish-2.jpg'
 import { ABOUT_TEXT } from '../constants'
-import hackimg from '../assets/Manish_Photo_Bomb-portfolio.png'
+import hackimg from '../assets/photo_collage.png'
 
 const About = () => {
   return (
@@ -20,7 +20,7 @@ const About = () => {
         <div className="w-full lg:w-1/2">
           <div className="flex justify-center items-center lg:justify-start flex-col">
             <p className='my-2 max-w-xl py-6'>{ABOUT_TEXT}</p>
-            <img className='rounded-2xl lg:mt-40 w-3/2 h-3/4' src={hackimg}/>
+            <img className='rounded-2xl lg:mt-40 w-3/4 h-1/2' src={hackimg}/>
             <div className="lg:hidden">
               <h2 className='font-medium mt-10 text-3xl px-12'>Engaged in <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-transparent'>15+ Hackathons</span> Across Diverse Locales, Blending Online And Offline Formats</h2>
               <p className='max-w-xl pt-4 pl-12'>I'm seasoned in hackathons, having participated in over 15 events, both online and offline, across various states. With each competition, I've honed my problem-solving skills and fostered innovation, consistently delivering impactful solutions under pressure.</p>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,12 +7,12 @@ const Projects = () => {
         <h1 className='my-20 text-center text-4xl'>Projects</h1>
         <div>
             {PROJECTS.map((project, index) => (
-                <div key={index} className='mb-20 flex flex-wrap lg:justify-center text-center lg:text-left lg:gap-20'>
+                <div key={index} className='mb-20 flex flex-wrap lg:justify-center lg:gap-20'>
                     <div className='w-full lg:w-1/4 flex justify-center items-center'>
                         <img src={project.image} width={300} height={300} alt={project.title}  className='mb-6 rounded-md'/>
                     </div>
-                    <div className="w-full max-w-xl lgw-3/4">
-                        <h6 className='mb-2 font-semibold text-2xl'>{project.title}</h6>
+                    <div className="w-full max-w-xl lgw-3/4 flex flex-wrap gap-4">
+                        <h6 className='mb-2 font-semibold text-2xl w-full text-center lg:w-screen lg:text-left'>{project.title}</h6>
                         <p className='mb-4 text-neutral-400'>{project.description}</p>
                         {project.technologies.map((tech,index) => (
                             <span key={index} className='mr-2 bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-900 rounded'>{tech}</span>
